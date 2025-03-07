ff4900d4c3e54792526aab5f7b18599dff072d48
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-03-07 07:41:58 +0100

    feat: remove lombock

--- blog-backend/src/main/java/ch/hftm/blogproject/model/dto/CommentDTO.java
+++ blog-backend/src/main/java/ch/hftm/blogproject/model/dto/CommentDTO.java
@@ -5,13 +5,7 @@ import java.time.ZonedDateTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.ALWAYS)
 public class CommentDTO {
 
@@ -22,4 +16,77 @@ public class CommentDTO {
     private String creator;
     private ZonedDateTime createdAt;
     private ZonedDateTime lastChangedAt;
+    private boolean validationStatus;
+
+    // Default constructor
+    public CommentDTO() {
+    }
+
+    // All-args constructor
+    public CommentDTO(Long commentID, Long blogID, String content, String creator, ZonedDateTime createdAt, ZonedDateTime lastChangedAt, boolean validationStatus) {
+        this.commentID = commentID;
+        this.blogID = blogID;
+        this.content = content;
+        this.creator = creator;
+        this.createdAt = createdAt;
+        this.lastChangedAt = lastChangedAt;
+        this.validationStatus = validationStatus;
+    }
+
+    // Getters and Setters
+    public Long getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(Long commentID) {
+        this.commentID = commentID;
+    }
+
+    public Long getBlogID() {
+        return blogID;
+    }
+
+    public void setBlogID(Long blogID) {
+        this.blogID = blogID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ZonedDateTime getLastChangedAt() {
+        return lastChangedAt;
+    }
+
+    public void setLastChangedAt(ZonedDateTime lastChangedAt) {
+        this.lastChangedAt = lastChangedAt;
+    }
+
+    public boolean getValidationStatus() {
+        return validationStatus;
+    }
+
+    public void setValidationStatus(boolean validationStatus) {
+        this.validationStatus = validationStatus;
+    }
 }
--- blog-backend/src/main/java/ch/hftm/blogproject/model/entity/Comment.java
+++ blog-backend/src/main/java/ch/hftm/blogproject/model/entity/Comment.java
@@ -7,14 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Comment {
 
     @Id
@@ -26,4 +20,77 @@ public class Comment {
     private String creator;
     private ZonedDateTime createdAt;
     private ZonedDateTime lastChangedAt;
+    private boolean validationStatus;
+
+    // Default constructor
+    public Comment() {
+    }
+
+    // All-args constructor
+    public Comment(Long commentID, Long blogID, String content, String creator, ZonedDateTime createdAt, ZonedDateTime lastChangedAt, boolean validationStatus) {
+        this.commentID = commentID;
+        this.blogID = blogID;
+        this.content = content;
+        this.creator = creator;
+        this.createdAt = createdAt;
+        this.lastChangedAt = lastChangedAt;
+        this.validationStatus = validationStatus;
+    }
+
+    // Getters and Setters
+    public Long getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(Long commentID) {
+        this.commentID = commentID;
+    }
+
+    public Long getBlogID() {
+        return blogID;
+    }
+
+    public void setBlogID(Long blogID) {
+        this.blogID = blogID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ZonedDateTime getLastChangedAt() {
+        return lastChangedAt;
+    }
+
+    public void setLastChangedAt(ZonedDateTime lastChangedAt) {
+        this.lastChangedAt = lastChangedAt;
+    }
+
+    public boolean getValidationStatus() {
+        return validationStatus;
+    }
+
+    public void setValidationStatus(boolean validationStatus) {
+        this.validationStatus = validationStatus;
+    }
 }
